142d955ff5c6134b3e98f832cc9f5ab3d4892ac2
RimakiTaema <txwfyxa@gmail.com>
2025-12-21 07:33:07 +0700

    Ready to Test lol

--- rewrite/backend/src/index.ts
+++ rewrite/backend/src/index.ts
@@ -1,11 +1,10 @@
 import { env } from "bun";
 import { Elysia } from "elysia";
-
+import { gc2_auth } from "./modules/auth/index"
 const port = Number(env.API_PORT ?? 3000);
 
 const gc2 = new Elysia()
   .use(gc2_auth)
-  .use(gc2_services)
   .listen(port);
 
 console.log(`🚀 API running on http://localhost:${port}`);
--- rewrite/backend/src/modules/auth/index.ts
+++ rewrite/backend/src/modules/auth/index.ts
@@ -2,15 +2,16 @@ import { Context, Elysia } from "elysia"
 import * as gc2crypt from "./crypt/module"
 import { env } from "bun"
 
-const AES_TECHNOLOGY = Bun.env.AES_TECHNOLOGY!
-const AES_ENCODING = Bun.env.AES_ENCODING!
+// const AES_TECHNOLOGY = Bun.env.AES_TECHNOLOGY!
+const AES_ENCODING = Bun.env.AES_ENCODING as BufferEncoding
 
 if (!Bun.env.AES_CBC_KEY)
     throw new Error("Missing AES_CBC_KEY, Did you set .env correctly?")
 if (!Bun.env.AES_CBC_IV)
     throw new Error("Missing AES_CBC_IV, Did you set .env correctly?")
 
-const AES_CBC_KEY = Buffer.from(Bun.env.AES_CBC_KEY, AES_ENCODING)
+const AES_CBC_KEY = Buffer.from(Bun.env.AES_CBC_KEY!, AES_ENCODING!)
+const AES_CBC_IV = Buffer.from(Bun.env.AES_CBC_IV)
 
 function rawpayload(ctx: Context): string{
     const url = ctx.request.url
@@ -21,11 +22,9 @@ function rawpayload(ctx: Context): string{
     return url.slice(payload + 1)
 }
 
-function 
-
 export const gc2_auth = new Elysia() // wHY I NEED TO LET IT BLANK WTH it's feels wrong
 gc2_auth.get('start.php', (ctx) => {
        const raw = rawpayload(ctx)
-
-       gc2crypt.decryptAES(raw, AES_CBC_KEY, AES_CBC_IV)
+       const decrypted = gc2crypt.decryptAES(raw, AES_CBC_KEY, AES_CBC_IV)
+       console.log(decrypted)
 })
